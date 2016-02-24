cde2e75b1ed5a843e93ea51b1068b23e702b6db5
henanzhg <854702773@qq.com>
2016-02-24 14:02:13 +0800

    Initial Commit

---
+++ Quartz2D_ObjC_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quartz2D_ObjC_Demo
+//
+//  Created by mac on 2/24/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quartz2D_ObjC_Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Quartz2D_ObjC_Demo
+//
+//  Created by mac on 2/24/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
